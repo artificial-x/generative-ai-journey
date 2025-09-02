def3bba6933fbf7ede5d16daa25b4495491fc52c
Saif Satti <artificialxagent@gmail.com>
2025-09-02 17:11:03 +0500

    Removed .env file and updated Day-2 chat_openai.py

--- Day-2/Chat-Models/chat_openai.py
+++ Day-2/Chat-Models/chat_openai.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 load_dotenv()
 
-model = ChatOpenAI(model="gpt-3.5-turbo",tempreture=0.3,max_completion_tokens=500)
+model = ChatOpenAI(model="gpt-3.5-turbo",max_completion_tokens=500)
 
 model.invoke("What is Capital of pakistan?")
 result = model.invoke("What is Capital of pakistan?")
